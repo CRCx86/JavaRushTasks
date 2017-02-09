76076a99cbaaeae1b778cd67c46f6c6e69b40de0
CRCx86 <crc32zinov@gmail.com>
2017-02-09 13:34:20 +0300

    1

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2408/Dog.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2408/Dog.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task24.task2408;
 
+import java.util.Date;
+
 /*
 В работе вам иногда будет нужно закастить класс к какому-нибудь другому классу, не интерфейсу :)))
 Класс DogPet использует 2 класса - SuperDog и Dog, разберись с getName в классе DogPet
@@ -42,6 +44,28 @@ public class Dog implements Pet {
      * @return экземпляр класса DogPet
      */
     public Sayable toSayable(final int i) {
-       return null;
+
+        class DogPet extends SuperDog implements Sayable {
+
+            @Override
+            public String say() {
+
+                if (i < 1) {
+                    return getName() + " спит.";
+                }else {
+                    String s = "г";
+                    for (int j = 0; j < i; j++) s += "а";
+                    s += "в";
+                    return getName() + " лает " + s + "! " + formatter.format(new Date());
+                }
+            }
+
+            private String getName() {
+
+                return getSuperQuotes() + name + getSuperQuotes();
+            }
+        }
+
+       return new DogPet();
     }
 }
